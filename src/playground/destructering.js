87293110b7af570343fad272686a85095721da18
JoeyHa <joey.havia@gmail.com>
2018-08-01 17:03:49 +0300

    Initial Commit

---
+++ src/playground/destructering.js
@@ -0,0 +1,36 @@
+/*  const person = {
+    name: 'Joey',
+    age: 26,
+    location: {
+        city: 'Holon',
+        temp: 55
+    }
+}; 
+
+const { name : firstName  = 'Anonymous ', age } = person;
+console.log(`${firstName} is  ${person.age}. `);
+
+const { city, temp: tempature } = person.location
+
+if (city && tempature) {
+console.log(`its ${tempature} in  ${city}.`);
+}
+ */
+/* 
+ const book = { 
+     title: 'Ego is the Enemy',
+     author: 'Ryan Holiday',
+     publisher: {
+         name: 'Penguin'
+     }
+ };
+
+ const {name: publisherName = 'self-publisher' } = book.publisher; 
+
+ console.log(publisherName) // Penguin , self-publish
+ */
+
+ //
+ // Array destructering
+ //
+ 
